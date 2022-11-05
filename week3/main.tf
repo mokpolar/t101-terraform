33905fd2d2e923dc5cf3eec2d9e6737fa3bd44fb
mokpolar <beholder16@gmail.com>
2022-11-05 22:01:41 +0900

    modify ec2ami to tokyo

--- week3/main.tf
+++ week3/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c76973fbe0ee100c"
+  ami           = "ami-078296f82eb463377"
   instance_type = "t2.micro"
   tags = {
     Name = "t101-week3"
